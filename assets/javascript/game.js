3c3dd625e644472a3ebf79f624f4aa8e0228fe33
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2019-10-17 11:49:28 -0400

    updated index.html begining of game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var userChoicez = [];
+
+var winzDiv = document.getElementById("winz");
+targetDiv.textContent = 0;
+
+var lossezDiv = document.getElementById("lossez");
+targetDiv.textContent = 0;
+
+var guessezDiv = document.getElementById("guessezLeft");
+targetDiv.textContent = 0;
+
+var letterzDiv = document.getElementById("letterz");
+targetDiv.textContent = 0;

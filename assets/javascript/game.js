15abc06f37228cc1f0a9105e99765f1eaac8e8bc
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2019-10-17 21:26:08 -0400

    added some vars, added some text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,39 @@
 //hopefully this is helpful
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerChoicez = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //empty div for later flex storage
 var userChoicez = [];
 
+//some divs for display elements
+var winz = 0;
+var lossez = 0;
+var guessez = 5;
+var guessezLeft = 5;
+
+
+// i need a variable to keep track of something...
+var letterToGuess = null;
+
+//ok, computer choice time
+//something, randome number generator, bounded by the length of the list, 26
+var computerGuessz = computerChoicez[Math.floor(Math.random() * computerChoicez.length)];
+
+//set a variable to a key press
+document.onkeyup = function (event) {
+    var userChoicez = ;
+    //set a variable to 
+
+}
+
 //i need to somehow update these HTML elements to display the current values
-var winzDiv = document.getElementById("winz");
-targetDiv.textContent = 0;
+//var winzDiv = document.getElementById("winz");
+//targetDiv.textContent = 0;
 
-var lossezDiv = document.getElementById("lossez");
-targetDiv.textContent = 0;
+//var lossezDiv = document.getElementById("lossez");
+//targetDiv.textContent = 0;
 
-var guessezDiv = document.getElementById("guessezLeft");
-targetDiv.textContent = 9;
+//var guessezDiv = document.getElementById("guessezLeft");
+//targetDiv.textContent = 9;
 
-var letterzDiv = document.getElementById("letterz");
-targetDiv.textContent = 0;
+//var letterzDiv = document.getElementById("letterz");
+//targetDiv.textContent = 0;

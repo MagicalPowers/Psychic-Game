400d5ab50a36afc37fa960fc0826ca8642edfe0e
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2019-10-21 16:57:59 -0400

    all of this is trash

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//what am i allowed to write/?
+//is it possible to write javascript that has never been written before? should i?
+//is it legal to copy something that I wrote? what if i was inspired? is this all trash? is it all stolen? 
+//why are you still reading this? What is your goal? 
+//id like to establish global variables, but that's been done by others.
+
+//id like to keep track of some variables to store information in, and use them to fill in some divs in my index.html, but i cant think of an orginal way to say that. 
+
+//how can i write logic and be original? 
+
